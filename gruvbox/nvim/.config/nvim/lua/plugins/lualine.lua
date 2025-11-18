1ed8e5a867270220ff2d0ebc7ee3e50ddec08f3a
eitanhanson <eitan.hanson@gmail.com>
2025-11-18 22:56:33 +0200

    WIP

--- gruvbox/nvim/.config/nvim/lua/plugins/lualine.lua
+++ gruvbox/nvim/.config/nvim/lua/plugins/lualine.lua
@@ -8,28 +8,6 @@ return {
 		require("lualine").setup({
 			options = {
 				theme = 'gruvbox'
-			},
-			sections = {
-				lualine_x = {
-					{
-						function()
-							local dap = require("dap")
-							local session = dap.session()
-							if session then
-								return " " .. session.config.name
-							end
-							return ""
-						end,
-						cond = function()
-							local dap = require("dap")
-							return dap.session() ~= nil
-						end,
-						color = { fg = "#fe8019" }, -- gruvbox orange
-					},
-					'encoding',
-					'fileformat',
-					'filetype'
-				},
 			}
 		})
 	end
