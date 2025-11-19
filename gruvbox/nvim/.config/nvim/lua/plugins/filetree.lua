007ee402bd26778fcb41f8e0f0a66534db3e080e
eitanhanson <eitan.hanson@gmail.com>
2025-11-19 10:38:09 +0200

    add window picker

--- gruvbox/nvim/.config/nvim/lua/plugins/filetree.lua
+++ gruvbox/nvim/.config/nvim/lua/plugins/filetree.lua
@@ -1,14 +1,21 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	lazy = false,
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		lazy = false,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+		config = function()
+			require("plugins.config.filetree")
+		end,
 	},
-	config = function()
-		require("plugins.config.filetree")
-	end,
+    {
+        "s1n7ax/nvim-window-picker",
+        config = function()
+            require("plugins.config.window-picker")
+        end,
+    }
 }
